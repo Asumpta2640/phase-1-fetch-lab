9306b8b49c0298c1b5a704d1f09d943cf07e8ce4
Jelagat2640 <sugutasumpta@gmail.com>
2025-06-21 19:11:57 +0300

    index.js

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 function fetchBooks() {
+  console.log("Calling fetchBooks...")
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+     .then(response =>{
+      if(!response.ok){
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+     })
+     .then(data =>{
+      console.log("Fetched books:",data);
+      renderBooks(data);
+     })
+     .catch(error=>{
+       console.error("Error during fetchBooka:",error);
+     });
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
+  if(!main){
+    console.error("No <main> element found in DOM.");
+    return;
+  }
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
